8f1c2e6352556420512b1b076d54f6889b95ee30
EliasKhoueiry <167768001+EliasKhoueiry@users.noreply.github.com>
2024-12-29 20:57:57 +0200

    website.js

---
+++ website.js
@@ -0,0 +1,9 @@
+function burger(){
+    var button1 = document.getElementById("popover")
+    if(button1.style.display ==="none"){
+        button1.style.display = "flex"
+    }
+    else{
+        button1.style.display = "none"
+    }
+}
